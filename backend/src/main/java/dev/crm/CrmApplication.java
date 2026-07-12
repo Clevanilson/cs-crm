357f287e58e80fb20f6fd0e202d339029e5ca265
Clevanilson <clevanilson.contato@gmail.com>
2026-07-12 14:13:16 -0300

    feat(backend): initial commit

---
+++ backend/src/main/java/dev/crm/CrmApplication.java
@@ -0,0 +1,13 @@
+package dev.crm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrmApplication.class, args);
+	}
+
+}
