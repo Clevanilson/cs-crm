c64960ee7a1ad98c2bb0d6673d8895a5ec381b24
Clevanilson <clevanilson.contato@gmail.com>
2026-07-13 20:50:46 -0300

    feat(backend): update `Client` entity
    - add id

--- backend/src/main/java/dev/crm/domain/entity/Client.java
+++ backend/src/main/java/dev/crm/domain/entity/Client.java
@@ -1,21 +1,37 @@
 package dev.crm.domain.entity;
 
+import java.util.Optional;
 import java.util.regex.Pattern;
 
 import dev.crm.domain.error.DomainError;
 
 public class Client {
+    private Optional<Long> id;
     private String name;
     private String email;
     private String phone;
+    
 
-    public Client(String name, String email, String phone) {
+    public Client(String name, String email, String phone, Optional<Long> id) {
         if (!this.isNameValid(name)) throw new DomainError("Invalid name");
         if (!this.isEmailValid(email)) throw new DomainError("Invalid email");
         if (!this.isPhoneValid(phone)) throw new DomainError("Invalid phone");
         this.name = name;
         this.email = email;
         this.phone = phone;
+        this.id = id;
+    }
+
+    public static Client create(String name, String email, String phone) {
+        return new Client(name, email, phone, Optional.empty());
+    }
+
+    public Optional<Long> getId() {
+        return this.id;
+    }
+
+    public void setId(Long id) {
+        this.id = Optional.of(id);
     }
 
     public String getName() {
--- backend/src/test/java/dev/crm/domain/entity/ClientTest.java
+++ backend/src/test/java/dev/crm/domain/entity/ClientTest.java
@@ -13,16 +13,23 @@ import dev.crm.domain.error.DomainError;
 public class ClientTest {
     @Test
     void createClientWithValidData() {
-        var sut = new Client("Verso Dessendre", "verso@crm.com", "1198833445566");
+        var sut =  Client.create("Verso Dessendre", "verso@crm.com", "1198833445566");
         assertEquals(sut.getName(), "Verso Dessendre");
         assertEquals(sut.getEmail(), "verso@crm.com");
         assertEquals(sut.getPhone(), "1198833445566");
     }
 
+    @Test
+    void setIdClientWithValidData() {
+        var sut = Client.create("Verso Dessendre", "verso@crm.com", "1198833445566");
+        sut.setId(1L);
+        assertEquals(sut.getId().get(), 1L);
+    }
+
     @ParameterizedTest
     @ValueSource(strings = {"a", "a2", ""})
     void createClientWithInvalidName(String name) {
-        var sut = assertThrows(DomainError.class, () -> new Client(name, "verso@crm.com", "1198833445566"));
+        var sut = assertThrows(DomainError.class, () ->  Client.create(name, "verso@crm.com", "1198833445566"));
         assertEquals(sut.getMessage(), "Invalid name");
     }
 
@@ -33,7 +40,7 @@ public class ClientTest {
         "verso"
     })
     void createClientWithInvalidEmail(String email) {
-        var sut = assertThrows(DomainError.class, () -> new Client("Verso", email, "1198833445566"));
+        var sut = assertThrows(DomainError.class, () ->  Client.create("Verso", email, "1198833445566"));
         assertEquals(sut.getMessage(), "Invalid email");
     }
 
@@ -45,7 +52,7 @@ public class ClientTest {
         "7998833445"
     })
     void createClientWithInvalidPhone(String phone) {
-        var sut = assertThrows(DomainError.class, () -> new Client("Verso", "verso@crm.com", phone));
+        var sut = assertThrows(DomainError.class, () ->  Client.create("Verso", "verso@crm.com", phone));
         assertEquals(sut.getMessage(), "Invalid phone");
     }
 
