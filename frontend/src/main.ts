9a696c7648ee3e1e2ac962591ad71c5b229c190e
Clevanilson <clevanilson.contato@gmail.com>
2026-07-15 20:01:36 -0300

    feat(frontend): register `ClientGateway` for DI

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -3,11 +3,15 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import './styles/main.css'
 import router from './router'
+import { ClientHttpGateway } from './infra/gateway/client-http.gateway.ts'
+import { FetchAdapter } from './http/fetch-adapter.ts'
 
 const app = createApp(App)
 
+const httpClient = new FetchAdapter()
+const ClientGateway = new ClientHttpGateway(httpClient)
+app.provide('clientGateway', ClientGateway)
 app.use(createPinia())
-
 router.push('/clients')
 app.use(router)
 app.mount('#app')
